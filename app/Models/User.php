092c741e39271e9da250e933cc09e2abe4ba8585
ViniciusCosta126 <viniciuscosta126@gmail.com>
2025-12-29 01:22:33 -0300

    Criando base de multiclinicas

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function clinica(){
+        return $this->belongsTo(Clinica::class);
+    }
 }
